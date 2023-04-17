dc75824a2adf67a36e158bc75bdf16f33a358540
UchegbuNonso <maxie505deprof@yahoo.com>
2023-04-17 22:46:16 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// console.log('Hello');
+const {v4: uuidv4} = require('uuid');
+// import { v4 as uuidv4 } from "uuid";  /*as seen in React */
+
+// (once done, you have to add a type property  after the name and give it a value of module in package.json  
+// use toptal node.js gitignore(browse it) to get list of git ignore files
+console.log(uuidv4())
+
